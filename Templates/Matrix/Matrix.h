9097337464c93b4dd73e170723edc1f705c9d6e3
SamuelYang1 <yangjun2@mail.ustc.edu.cn>
2016-10-17 20:59:38 +0800

    Update Matrix.h

--- Templates/Matrix/Matrix.h
+++ Templates/Matrix/Matrix.h
@@ -1,7 +1,10 @@
-const int cc=0x7FFFFFFF,MAXN=1e3+1;//模数 
+//const long long cc=0x7FFFFFFFFFFFFFFF;//模数 
+const long long cc=0x7FFFFFFF;//模数2 
+const int MAXN=101;//最大行列数，不要设置太大，因为没写拷贝函数，拷贝时会超时 
 struct Matrix
 {
-    int r,c,w[MAXN][MAXN];//r是行数，c是列数，下标从1开始 
+    int r,c;//r是行数，c是列数，下标从1开始
+	long long w[MAXN][MAXN]; 
 };
 Matrix operator+(const Matrix &A,const Matrix &B)//加法，减法对此复制即可 
 {
@@ -27,7 +30,7 @@ Matrix operator*(const Matrix &A,const Matrix &B)//乘法
 	 }
 	return C;
 }
-Matrix pow(Matrix A,int k)//快速幂 
+Matrix pow(Matrix A,int k)//快速幂 非递归版 
 {
 	Matrix C;
 	C.c=A.c;
@@ -44,9 +47,26 @@ Matrix pow(Matrix A,int k)//快速幂
 	}
 	return C;
 }
-int tr(Matrix A)//迹 
+Matrix powsum(Matrix A,int k,Matrix &C)//快速幂求和 
 {
-	int tr=0;
+	if (k==1)
+	{
+		C=A;
+		return C;
+	}
+	Matrix B=powsum(A,k/2,C);
+	B=B+C*B;
+	if (k&1) 
+	{
+	    C=C*C*A;
+		B=B+C;
+    }
+	else C=C*C;
+	return B;
+}
+long long tr(Matrix A)//迹 
+{
+	long long tr=0;
 	for (int i=1;i<=A.r;i++) tr=(A.w[i][i]+tr)%cc;
 	return tr;
 }
